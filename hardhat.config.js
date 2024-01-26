be1985fec279f9cf871dce6355c3f7b84caeb5a0
Tang Wei <tw7613781@gmail.com>
2024-01-26 10:58:56 +0800

    import contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,21 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.6.10",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://bsc-dataseed1.binance.org/",
+        blockNumber: 35570649
+      }
+    }
+  }
 };
